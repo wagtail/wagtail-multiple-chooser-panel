590cf2173cb1ece8fbcbf43e780c0a82029b2dae
Matt Westcott <matt@west.co.tt>
2022-12-05 15:47:18 +0000

    Add mechanism for populating choosers with the data from the modal

--- wagtail_multiple_chooser_panel/static_src/main.js
+++ wagtail_multiple_chooser_panel/static_src/main.js
@@ -6,14 +6,20 @@ class MultipleChooserPanel extends InlinePanel {
     constructor(opts) {
         super(opts);
 
-        const chooserWidgetFactory = window.telepath.unpack(
+        this.chooserWidgetFactory = window.telepath.unpack(
             JSON.parse(document.getElementById(`${ opts.formsetPrefix }-CHOOSER_WIDGET`).textContent)
         );
+
         $(`#${ opts.formsetPrefix }-OPEN_MODAL`).on('click', () => {
-            chooserWidgetFactory.openModal(
+            this.chooserWidgetFactory.openModal(
                 (result) => {
                     result.forEach(item => {
                         this.addForm();
+                        const formIndex = this.formCount - 1;
+                        const formPrefix = `${ opts.formsetPrefix }-${ formIndex }`;
+                        const chooserFieldId = `${ formPrefix }-${ opts.chooserFieldName }`
+                        const chooserWidget = this.chooserWidgetFactory.getById(chooserFieldId);
+                        chooserWidget.setStateFromModalData(item);
                     });
                 },
                 {multiple: true}
