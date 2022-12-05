7bf4f8427e2bbb95b7f536630dc21c066a1bbf4f
Matt Westcott <matt@west.co.tt>
2022-12-05 14:38:53 +0000

    Move onclick handler into MultipleChooserPanel constructor and create the appropriate number of forms

--- wagtail_multiple_chooser_panel/static_src/main.js
+++ wagtail_multiple_chooser_panel/static_src/main.js
@@ -1,6 +1,25 @@
+import $ from 'jquery';
+
 import { InlinePanel } from './InlinePanel';
 
 class MultipleChooserPanel extends InlinePanel {
+    constructor(opts) {
+        super(opts);
+
+        const chooserWidgetFactory = window.telepath.unpack(
+            JSON.parse(document.getElementById(`${ opts.formsetPrefix }-CHOOSER_WIDGET`).textContent)
+        );
+        $(`#${ opts.formsetPrefix }-OPEN_MODAL`).on('click', () => {
+            chooserWidgetFactory.openModal(
+                (result) => {
+                    result.forEach(item => {
+                        this.addForm();
+                    });
+                },
+                {multiple: true}
+            );
+        });
+    }
 }
 
 window.MultipleChooserPanel = MultipleChooserPanel;
